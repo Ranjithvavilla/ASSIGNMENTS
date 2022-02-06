5a0d09efc76c4642df95ce53b4778f07abda793f
Ranjithvavilla <90271776+Ranjithvavilla@users.noreply.github.com>
2022-02-06 19:25:21 +0530

    Add files via upload

---
+++ assignment-26jan/script.js
@@ -0,0 +1,12 @@
+var num1=prompt("enter the number");
+var num2=prompt("enter the number");
+var num3=prompt("enter the number");
+if(num1<num2 && num1<num3){
+alert("num1 is minimum");
+}
+else if(num2<num1 && num2<num3){
+alert("num2 is minimum");
+}
+else{
+alert("num3 is minimum");
+}
---
+++ assignment-28jan/index.js
@@ -0,0 +1,8 @@
+var ip=prompt("Enter the Any number: ");
+var ip1=parseInt(ip);
+
+console.log("Given number by user is : "+ip1);
+for(var ctr=ip1; ctr>=0; ctr=ctr-1)
+{
+    console.log(ctr);
+}
